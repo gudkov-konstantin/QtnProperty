91e7d03c52f1ca36e15342228ddc8393d0dbd7d2
gudkov-konstantin <gudkov.danger@gmail.com>
2026-04-07 12:28:05 +0700

    MyColor:operator== for std::equal_to

--- Demo/AB/PropertyABColor.h
+++ Demo/AB/PropertyABColor.h
@@ -46,6 +46,9 @@ struct MyColor
     int red = 0;
     int green = 0;
     int blue = 0;
+    bool operator==(const MyColor &rhs) const {
+        return this->red == rhs.red && this->green == rhs.green && this->blue == rhs.blue;
+    }
 };
 
 using QtnPropertyMyColorBase = QtnSinglePropertyBaseAs<QtnPropertyQColorBase, MyColor>;
